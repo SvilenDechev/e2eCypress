1898118d79235129dfe15a54aaef42fe6444b4a9
Svilen Dechev <sdechev@asteasolutions.com>
2022-04-15 10:47:26 +0300

    upgrade_cypress_applitools

--- applitools.config.js
+++ applitools.config.js
@@ -1,10 +1,17 @@
-import key from "./cypress/fixtures/applitoolsKey.json"
 module.exports = {
   testConcurrency: 1,
-  apiKey: key.applitoolsKey,
+  apiKey: 'q8hQ88tr105YHY9110KwEgY2z3xcCBchWks8dcjoTboX1BE110',
   browser: [
-      {width: 800, height: 600, name: 'chrome'},
+      // Add browsers with different viewports
+      {width: 1980, height: 1020, name: 'chrome'},
+      // {width: 700, height: 500, name: 'firefox'},
+      // {width: 1600, height: 1200, name: 'ie11'},
+      // {width: 1024, height: 768, name: 'edgechromium'},
+      // {width: 800, height: 600, name: 'safari'},
+      // // Add mobile emulation devices in Portrait mode
+      // {deviceName: 'iPhone X', screenOrientation: 'portrait'},
+      // {deviceName: 'Pixel 2', screenOrientation: 'portrait'}
   ],
   // set batch name to the configuration
-  batchName: 'First Test with Applitools'
+  batchName: 'applitools test example'
 }
--- cypress/integration/screenComparising.Spec.js
+++ cypress/integration/screenComparising.Spec.js
@@ -1,39 +1,31 @@
-xit('screen', () => {
-  cy.visit('/todo')
-
-
-  cy.eyesOpen({
-    appName: 'demo app',
-    testName: 'test grid demo',
-});
-
-cy.eyesCheckWindow({
-  tag: "Login Window",
-  target: 'window',
-  fully: true
-});
-// cy.get('#log-in').click()
-
-// cy.eyesCheckWindow({
-//   tag: "Home page and grid",
-//   target: 'window',
-//   fully: true
-// });
-
-cy.eyesClose()
-});
-
-it('UI visual test', () => {
-  cy.visit('/todo')
-  cy.findByPlaceholderText('What needs to be done?').should('be.visible');
-  cy.eyesOpen({
-    appName:'todoApp',
-    tagName: 'test first state of the app, should have two todo '
+describe('applitools test', () => {
+  beforeEach(() => {
+    cy.eyesOpen({
+      appName: 'AsteaSolutions ',
+      testName: 'Astea Web',
+    });
   });
-  cy.eyesCheckWindow({
-    tag:'todoHomePage',
-    target: 'window',
-    fully: true
+  it('screen', () => {
+    cy.visit('https://abv.bg/')
+
+    cy.eyesCheckWindow({
+      tag: "Home Window",
+      target: 'window',
+      fully: true
+    });
+
+    // cy.get('#log-in').click()
+
+    // cy.eyesCheckWindow({
+    //   tag: "Home page and grid",
+    //   target: 'window',
+    //   fully: true
+    // });
   });
-  cy.eyesClose();
-})
+  afterEach(() => {
+    cy.eyesClose()
+  });
+});
+
+
+
